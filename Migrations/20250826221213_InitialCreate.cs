eafeebb0f6c1668a56fcde72b7b816ea575674b2
Floren-teena <florentinaantigha27@gmail.com>
2025-08-27 22:16:43 +0100

    chore: Created initial EF Core migrations for Transactions table

---
+++ Migrations/20250826221213_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TransactionAssessment.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    TransactionId = table.Column<Guid>(type: "uuid", nullable: false),
+                    AccountNumber = table.Column<string>(type: "character varying(12)", maxLength: 12, nullable: true),
+                    BeneficiaryName = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    BankName = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    SWIFTCode = table.Column<string>(type: "character varying(11)", maxLength: 11, nullable: true),
+                    Amount = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.TransactionId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transactions");
+        }
+    }
+}
