828346ac115b6f49ca749f0f7dae253c39b4fde9
Floren-teena <florentinaantigha27@gmail.com>
2025-08-27 22:05:27 +0100

    feat: Created Transaction model and view model to define the Transaction entity with its properties and data annotations

---
+++ Models/Transaction.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TransactionAssessment.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public Guid TransactionId { get; set; } = Guid.NewGuid();
+
+        [MaxLength(12)]
+        public string? AccountNumber { get; set; }
+
+        [MaxLength(100)]
+        public string? BeneficiaryName { get; set; }
+
+        [MaxLength(100)]
+        public string? BankName { get; set; }
+
+        [MaxLength(11)]
+        public string? SWIFTCode { get; set; }
+
+        public int Amount { get; set; }
+    }
+}
---
+++ Models/TransactionViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TransactionAssessment.Models
+{
+    public class TransactionViewModel
+    {
+        public Guid? TransactionId { get; set; }
+
+        [Required(ErrorMessage = "Account number is required")]
+        [StringLength(12, ErrorMessage = "Account number cannot exceed 12 characters")]
+        public string? AccountNumber { get; set; }
+
+        [Required(ErrorMessage = "Beneficiary name is required")]
+        [StringLength(100, ErrorMessage = "Beneficiary name cannot exceed 100 characters")]
+        public string? BeneficiaryName { get; set; }
+
+        [Required(ErrorMessage = "Bank name is required")]
+        [StringLength(100, ErrorMessage = "Bank name cannot exceed 100 characters")]
+        public string? BankName { get; set; }
+
+        [Required(ErrorMessage = "SWIFT code is required")]
+        [StringLength(11, ErrorMessage = "SWIFT code cannot exceed 11 characters")]
+        public string? SWIFTCode { get; set; }
+
+        [Required(ErrorMessage = "Amount is required")]
+        [Range(1, int.MaxValue, ErrorMessage = "Amount must be greater than 0")]
+        public int Amount { get; set; }
+    }
+}
