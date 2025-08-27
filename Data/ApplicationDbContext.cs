f93c7d70f2cd101bf041d078cae749089d209f73
Floren-teena <florentinaantigha27@gmail.com>
2025-08-27 22:07:28 +0100

    feat: Created the ApplicationDbContext to manage the entity set and database connection

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,8 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using TransactionAssessment.Models;
 
 namespace TransactionAssessment.Data
 {
     public class ApplicationDbContext: DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+        }
+
+        public DbSet<Transaction> Transactions { get; set; }
     }
 }
