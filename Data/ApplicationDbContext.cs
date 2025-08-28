a6536b0e0894a8eca8de0605a9d6318b788b05eb
Floren-teena <florentinaantigha27@gmail.com>
2025-08-28 12:24:00 +0100

    build: Updated ApplicationDbContext to implement IDataProtectionKeyContext and added a new migration to create the DataProtectionKeys table

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,14 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.DataProtection.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using TransactionAssessment.Models;
 
 namespace TransactionAssessment.Data
 {
-    public class ApplicationDbContext: DbContext
+    public class ApplicationDbContext: DbContext, IDataProtectionKeyContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
         {
         }
 
         public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<DataProtectionKey> DataProtectionKeys { get; set; }
     }
 }
--- Migrations/20250828111458_AddDataProtectionKeysTable.Designer.cs
+++ Migrations/20250828111458_AddDataProtectionKeysTable.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TransactionAssessment.Data;
@@ -11,9 +12,11 @@ using TransactionAssessment.Data;
 namespace TransactionAssessment.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250828111458_AddDataProtectionKeysTable")]
+    partial class AddDataProtectionKeysTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
